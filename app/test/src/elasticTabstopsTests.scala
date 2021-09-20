f63693c706715e3e339d8b7954c0674b2695d617
Cédric Thireau <cedric.thireau@hotmail.fr>
2021-09-20 20:02:30 +0200

    wip

--- app/test/src/elasticTabstopsTests.scala
+++ app/test/src/elasticTabstopsTests.scala
@@ -6,60 +6,60 @@ import elasticTabstops.{calcElasticTabstopPositions, spacesToSpaceTabs, spaceTab
 class ElasticTabstopsSpec extends FlatSpec {
 
   "spaceTabsToSpaces" should "replace space tabs with spaces correctly" in {
-    assert(spaceTabsToSpaces(" \t\ty", 4) == "    \ty")
-    assert(spaceTabsToSpaces("x \t\ty", 4) == "x   \ty")
-    assert(spaceTabsToSpaces("xxxxxxx \t\ty", 4) == "xxxxxxx  \ty")
+    assert(spaceTabsToSpaces("\ty", 4) == "    y")
+    assert(spaceTabsToSpaces("x \ty", 4) == "x   y")
+    assert(spaceTabsToSpaces("xxxxxxx \ty", 4) == "xxxxxxx  y")
 
     val given = List(
-      " \t\ty",
-      "xxxxxxx \t\ty"
+      " \ty",
+      "xxxxxxx \ty"
     ).mkString("\n")
 
     val expected = List(
-      "         \ty",
-      "xxxxxxx  \ty"
+      "         y",
+      "xxxxxxx  y"
     ).mkString("\n")
 
     assert(spaceTabsToSpaces(given, 4) == expected)
 
     val given2 = List(
       " \t",
-      "xxxxxxx\t"
+      "xxxxxxx"
     ).mkString("\n")
 
     val expected2 = List(
       "",
-      "xxxxxxx\t"
+      "xxxxxxx"
     ).mkString("\n")
 
     assert(spaceTabsToSpaces(given2, 4) == expected2)
   }
 
   "spacesToTabs" should "replace spaces with tabs correctly" in {
-    assert(spacesToSpaceTabs("    \ty", 4) == " \t\ty")
-    assert(spacesToSpaceTabs("x   \ty", 4) == "x \t\ty")
-    assert(spacesToSpaceTabs("xxxxxxx  \ty", 4) == "xxxxxxx \t\ty")
+    assert(spacesToSpaceTabs("    y", 4) == "\ty")
+    assert(spacesToSpaceTabs("x   y", 4) == "x \ty")
+    assert(spacesToSpaceTabs("xxxxxxx  y", 4) == "xxxxxxx \ty")
 
     val given = List(
-      "         \ty",
-      "xxxxxxx  \ty"
+      "         y",
+      "xxxxxxx  y"
     ).mkString("\n")
 
     val expected = List(
-      " \t\ty",
-      "xxxxxxx \t\ty"
+      " \ty",
+      "xxxxxxx \ty"
     ).mkString("\n")
 
     assert(spacesToSpaceTabs(given, 4) == expected)
 
     val given2 = List(
       "       ",
-      "xxx\t"
+      "xxx"
     ).mkString("\n")
 
     val expected2 = List(
       "",
-      "xxx\t"
+      "xxx"
     ).mkString("\n")
 
     assert(spacesToSpaceTabs(given2, 4) == expected2)
